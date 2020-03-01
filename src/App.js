ed30779d40763f7a49472a31df4b3cb0caee54d3
Ramis Vakazov <vakazov@restream.rt.ru>
2020-03-01 18:30:47 +0300

    convert to react hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, Fragment } from 'react'
 import axios from 'axios'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
@@ -15,126 +15,107 @@ const clientId = process.env.REACT_APP_GITHUB_CLIENT_ID
 const clientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET
 const client = `client_id=${clientId}&client_secret=${clientSecret}`
 
-export default class App extends Component {
-    state = {
-        users: [],
-        user: {},
-        repos: [],
-        loading: false,
-        hasError: false,
-        alert: null
+const App = () => {
+    const [users, setUsers] = useState([])
+    const [user, setUser] = useState({})
+    const [repos, setRepos] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [hasError, setError] = useState(false)
+    const [alert, setAlerts] = useState(null)
+
+    const userRequest = (userName) => axios.get(`https://api.github.com/users/${userName}?${client}`)
+    const userRepoRequest = (userName) => axios.get(`https://api.github.com/users/${userName}/repos?per_page=5&sort=created:asc&${client}`)
+    const searchUsersRequest = (text) => axios.get(`https://api.github.com/search/users?q=${text}&${client}`)
+
+    const setLoadingAndError = ({ loading, error }) => {
+        setLoading(loading)
+        setError(error)
     }
 
-    userRequest = (userName) => axios.get(`https://api.github.com/users/${userName}?${client}`)
-    userRepoRequest = (userName) => axios.get(`https://api.github.com/users/${userName}/repos?per_page=5&sort=created:asc&${client}`)
-    searchUsersRequest = (text) => axios.get(`https://api.github.com/search/users?q=${text}&${client}`)
-
-    getUsers = async (users) => {
-        this.setState({
-            loading: true,
-            hasError: false
-        })
+    const getUsers = async (users) => {
+        setLoadingAndError({ loading: true, error: false })
         try {
-            this.setState({
-                users: await users,
-                loading: false,
-                hasError: false
-            })
+            setUsers(await users)
+            setLoadingAndError({ loading: false, error: false })
         } catch (err) {
-            this.setState({
-                users: [],
-                loading: false,
-                hasError: true,
-                alert: {msg: 'An Error!', type: 'danger'}
-            })
+            setUsers([])
+            setLoadingAndError({ loading: false, error: true })
+            setAlert({msg: 'An Error!', type: 'danger'})
         }
     }
 
-    getUser = async (userName) => {
-        this.setState({
-            loading: true
-        })
+    const getUser = async (userName) => {
+        setLoadingAndError({ loading: true, error: false })
         try {
-            this.setState({
-                user: (await this.userRequest(userName)).data,
-                loading: false
-            })
+            setUser((await userRequest(userName)).data)
+            setLoadingAndError({ loading: false, error: false })
         } catch (err) {
-            this.setState({
-                user: [],
-                loading: false,
-                alert: {msg: 'An Error!', type: 'danger'}
-            })
+            setUser({})
+            setLoadingAndError({ loading: false, error: true })
+            setAlert({msg: 'An Error!', type: 'danger'})
         }
     }
 
-    getUserRepo = async (userName) => {
-        this.setState({
-            loading: true
-        })
+    const getUserRepo = async (userName) => {
+        setLoadingAndError({ loading: true, error: false })
         try {
-            this.setState({
-                repos: (await this.userRepoRequest(userName)).data,
-                loading: false
-            })
+            setRepos((await userRepoRequest(userName)).data)
+            setLoadingAndError({ loading: false, error: false })
         } catch (err) {
-            this.setState({
-                user: [],
-                loading: false,
-                alert: {msg: 'An Error!', type: 'danger'}
-            })
+            setRepos([])
+            setLoadingAndError({ loading: false, error: true })
+            setAlert({msg: 'An Error!', type: 'danger'})
         }
     }
 
-    searchUsers = async (text) => {
-        this.getUsers((await this.searchUsersRequest(text)).data.items)
+    const searchUsers = async (text) => {
+        getUsers((await searchUsersRequest(text)).data.items)
     }
 
-    clearUsers = () => {
-        this.setState({ users: [], loading: false, alert: null })
+    const clearUsers = () => {
+        setUsers([])
+        setLoadingAndError({ loading: false, error: false })
+        setAlerts(null)
     }
 
-    setAlert = (msg, type, timeout = 3500) => {
-        this.setState({ alert: { msg, type } })
-        setTimeout(() => this.setState({ alert: null }), timeout)
+    const setAlert = (msg, type, timeout = 3500) => {
+        setAlerts({ alert: { msg, type } })
+        setTimeout(() => setAlerts(null), timeout)
     }
 
-    render () {
-        const { loading, users, hasError, alert, user, repos } = this.state
-        return (
-            <Router>
-                <div className="App">
-                    <Navbar />
-                    <div className="container">
-                        <Alert alert={ alert } />
-                        <Switch>
-                            <Route exact path='/' render={props => (
-                                <Fragment>
-                                    <Search
-                                        searchUsers={ this.searchUsers }
-                                        clearUsers={ this.clearUsers }
-                                        showClear={ users.length > 0 }
-                                        setAlert={ this.setAlert }
-                                    />
-                                    <Users error={hasError} loading={loading} users={users} />
-                                </Fragment>
-                            )} />
-                            <Route exact path='/about' component={About} />
-                            <Route exact path='/user/:login' render={props => (
-                                <User {...props}
-                                      getUser={this.getUser}
-                                      getUserRepos={this.getUserRepo}
-                                      user={user}
-                                      repos={repos}
-                                      loading={loading}
+    return (
+        <Router>
+            <div className="App">
+                <Navbar />
+                <div className="container">
+                    <Alert alert={ alert } />
+                    <Switch>
+                        <Route exact path='/' render={props => (
+                            <Fragment>
+                                <Search
+                                    searchUsers={ searchUsers }
+                                    clearUsers={ clearUsers }
+                                    showClear={ users.length > 0 }
+                                    setAlert={ setAlert }
                                 />
-                            )} />
-                        </Switch>
-                    </div>
+                                <Users error={hasError} loading={loading} users={users} />
+                            </Fragment>
+                        )} />
+                        <Route exact path='/about' component={About} />
+                        <Route exact path='/user/:login' render={props => (
+                            <User {...props}
+                                  getUser={getUser}
+                                  getUserRepos={getUserRepo}
+                                  user={user}
+                                  repos={repos}
+                                  loading={loading}
+                            />
+                        )} />
+                    </Switch>
                 </div>
-            </Router>
-        )
-  }
+            </div>
+        </Router>
+    )
 }
 
-
+export default App
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,50 +1,46 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import PropTypes from 'prop-types'
 
-export default class Search extends Component {
-    state = {
-        text: ''
-    }
+const Search = ({ clearUsers, showClear, searchUsers, setAlert }) => {
+    const [text, setText] = useState('')
 
-    static propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-        setAlert: PropTypes.func.isRequired
-    }
-
-    onSubmit = (event) => {
+    const onSubmit = (event) => {
         event.preventDefault()
-        const { text } = this.state
         if (text !== '') {
-            this.props.searchUsers(text)
-            this.setState({ text: '' })
+            searchUsers(text)
+            setText('')
         } else {
-            this.props.setAlert('Please, enter something', 'light')
+            setAlert('Please, enter something', 'light')
         }
     }
 
-    onChange = ({ target: {name, value} }) => this.setState({ [name]: value })
+    const onChange = e => setText(e.target.value)
 
-    render() {
-        const { clearUsers, showClear } = this.props
-        return (
-            <div>
-                <form className="form" onSubmit={ this.onSubmit } >
-                    <input
-                        type="text"
-                        name="text"
-                        value={this.state.text}
-                        onChange={this.onChange}
-                        placeholder="Search Users..."
-                    />
-                    <input type="submit" value="Search" className="btn btn-dark btn-block" />
-                </form>
-                {
-                    showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>
-                }
+    return (
+        <div>
+            <form className="form" onSubmit={ onSubmit } >
+                <input
+                    type="text"
+                    name="text"
+                    value={text}
+                    onChange={onChange}
+                    placeholder="Search Users..."
+                />
+                <input type="submit" value="Search" className="btn btn-dark btn-block" />
+            </form>
+            {
+                showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>
+            }
 
-            </div>
-        )
-    }
+        </div>
+    )
+}
+
+export default Search
+
+Search.propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
 }
